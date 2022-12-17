cd8e5959ee4b87391f7fd6484007ec33725098a4
徐国彩 <xgc030920@outlook.com>
2022-12-17 17:23:00 +0800

    SPI_FLASH储存

---
+++ 16.SPI-读写串行FLASH/User/flash/bsp_spi_flash.h
@@ -0,0 +1,73 @@
+# ifndef _BSP_SPI_SLASH_H
+# define _BSP_SPI_FLASH_H
+
+# include "stm32f10x.h"
+# include "stdio.h"
+
+// 串口1-USART1
+#define  SPI_FLASHx                     SPI1
+#define  SPI_FLASH_CLK                  RCC_APB2Periph_SPI1
+#define  SPI_FLASH_APBxClkCmd           RCC_APB2PeriphClockCmd
+
+// SPI GPIO 引脚宏定义
+#define  SPI_FLASH_GPIO_CLK             RCC_APB2Periph_GPIOA
+#define  SPI_FLASH_GPIO_APBxClkCmd      RCC_APB2PeriphClockCmd
+    
+#define  SPI_FLASH_SCK_GPIO_PORT        GPIOA   
+#define  SPI_FLASH_SCK_GPIO_PIN         GPIO_Pin_5
+
+#define  SPI_FLASH_MOSI_GPIO_PORT       GPIOA
+#define  SPI_FLASH_MOSI_GPIO_PIN        GPIO_Pin_7
+
+#define  SPI_FLASH_MISO_GPIO_PORT       GPIOA
+#define  SPI_FLASH_MISO_GPIO_PIN        GPIO_Pin_6
+
+#define  SPI_FLASH_CS_GPIO_PORT         GPIOA
+#define  SPI_FLASH_CS_GPIO_PIN          GPIO_Pin_4
+
+# define SPI_FLASH_CS_LOW()             GPIO_ResetBits(SPI_FLASH_CS_GPIO_PORT, GPIO_Pin_4);
+# define SPI_FLASH_CS_HIGH()             GPIO_SetBits(SPI_FLASH_CS_GPIO_PORT, GPIO_Pin_4);
+
+# define SPI_FLASH_WAIT_TIMEOUT         10000
+
+/*命令定义-开头*******************************/
+#define W25X_WriteEnable		        0x06 //允许写使能
+#define W25X_WriteDisable		        0x04 //禁止写使能
+#define W25X_ReadStatusReg		        0x05 //读取状态寄存器S0 - S7
+#define W25X_WriteStatusReg		        0x01 
+#define W25X_ReadData			        0x03 //读资料
+#define W25X_FastReadData		        0x0B //读资料(比较快)
+#define W25X_FastReadDual		        0x3B 
+#define W25X_PageProgram		        0x02 //写入数据
+#define W25X_BlockErase			        0xD8 
+#define W25X_SectorErase		        0x20 //擦除4KB
+#define W25X_ChipErase			        0xC7 
+#define W25X_PowerDown			        0xB9 //进入一个低功耗模式
+#define W25X_ReleasePowerDown	        0xAB //退出低功耗模式
+#define W25X_DeviceID			        0xAB 
+#define W25X_ManufactDeviceID   	    0x90 
+#define W25X_JedecDeviceID		        0x9F
+
+/*调试串口配置*/
+void SPI_GPIO_Config(void);
+
+/*发送一个字节*/
+uint8_t SPI_FLASH_Send_Data(uint8_t data);
+
+/*接收一个字节*/
+uint8_t SPI_FLASH_Receive_Data(void);
+
+/*读取JEDEC_ID*/
+uint32_t SPI_FLASH_Read_JEDEC_ID(void);
+
+/*擦除操作*/
+void SPI_FLASH_Erase_Sector(uint32_t addr);
+
+/*读取操作*/
+void SPI_FLASH_Read_Buffer(uint32_t addr, uint8_t *data, uint32_t size);
+
+/*写入操作*/
+/*size 可以等于256 但不能大于*/
+void SPI_FLASH_Page_Write(uint32_t addr, uint8_t *data, uint32_t size);
+
+# endif
---
+++ 16.SPI-读写串行FLASH/User/main.c
@@ -0,0 +1,187 @@
+# include "stm32f10x.h"
+# include "./flash/bsp_spi_flash.h"
+# include "./uart/bsp_uart.h"
+
+/*1 普通存储*/
+/*0 长整数，浮点数存储*/
+
+#if 0
+uint8_t write_temp[4096] = {0};
+uint8_t read_temp[4096] = {0};
+
+int main(){
+
+	uint32_t read_id = 0;
+	uint32_t i;
+
+	/*调试串口配置*/
+	DEBUG_UART_Config();
+
+	/*调试串口配置*/
+	SPI_GPIO_Config();
+
+	printf("\r\n欢迎使用野火开发板\r\n");
+
+	read_id = SPI_FLASH_Read_JEDEC_ID();
+
+	printf("\r\nread_id = %x\r\n", read_id);
+	
+	SPI_FLASH_Erase_Sector(0);
+	
+	printf("\r\n擦除完毕\r\n");
+
+	for (i = 0; i < 4096; i ++){
+		write_temp[i] = i;
+	}
+
+	/*写入操作*/
+	SPI_FLASH_Page_Write(0, write_temp, 4096);
+	printf("\r\n写入完毕\r\n");
+
+	SPI_FLASH_Read_Buffer(0, read_temp, 4096);
+
+	printf("\r\n读取到的数据:\r\n");
+	for (i = 0; i < 4096; i ++){
+		printf("%x ", read_temp[i]);
+	}
+	printf("\r\n");
+
+	for (i = 0; i < 4096; i ++){
+		if (write_temp[i] != read_temp[i]){
+			printf("\r\n读写不一致\r\n");
+			break;
+		}
+	}
+	printf("\r\n校验结束\r\n");
+	
+	while(1);
+}
+
+#else 
+
+/*1 长整数存储*/
+/*0 浮点数存储*/
+
+#if 0
+
+int main(){
+
+	uint16_t write_temp = 520;
+	uint8_t read_temp[sizeof(write_temp)] = {0};
+
+	uint32_t read_id = 0;
+
+	/*调试串口配置*/
+	DEBUG_UART_Config();
+
+	/*调试串口配置*/
+	SPI_GPIO_Config();
+
+	printf("\r\n欢迎使用野火开发板\r\n");
+
+	read_id = SPI_FLASH_Read_JEDEC_ID();
+
+	printf("\r\nread_id = %x\r\n", read_id);
+	
+	/*擦除操作*/
+	SPI_FLASH_Erase_Sector(0);
+	
+	printf("\r\n擦除完毕\r\n");
+
+	/*写入操作*/
+	SPI_FLASH_Page_Write(0, (uint8_t*)&write_temp, sizeof(write_temp));
+	printf("\r\n写入完毕\r\n");
+
+	SPI_FLASH_Read_Buffer(0, read_temp, sizeof(write_temp));
+
+	printf("\r\n读取到的数据:\r\n");
+	printf("read_temp = %d", *(uint16_t *)read_temp);
+	
+	while(1);
+}
+
+#else
+
+/*1 浮点数存储*/
+/*0 字符串存储*/
+
+#if 0
+int main(){
+
+	double write_temp = 13.14;
+	uint8_t read_temp[sizeof(write_temp)] = {0};
+
+	uint32_t read_id = 0;
+
+	/*调试串口配置*/
+	DEBUG_UART_Config();
+
+	/*调试串口配置*/
+	SPI_GPIO_Config();
+
+	printf("\r\n欢迎使用野火开发板\r\n");
+
+	read_id = SPI_FLASH_Read_JEDEC_ID();
+
+	printf("\r\nread_id = %x\r\n", read_id);
+	
+	/*擦除操作*/
+	SPI_FLASH_Erase_Sector(0);
+	
+	printf("\r\n擦除完毕\r\n");
+
+	/*写入操作*/
+	SPI_FLASH_Page_Write(0, (uint8_t*)&write_temp, sizeof(write_temp));
+	printf("\r\n写入完毕\r\n");
+
+	SPI_FLASH_Read_Buffer(0, read_temp, sizeof(write_temp));
+
+	printf("\r\n读取到的数据:\r\n");
+	printf("read_temp = %.2lf", *(double *)read_temp);
+	
+	while(1);
+}
+
+#else
+
+int main(){
+
+	char write_temp[100] = "你好世界";
+	uint8_t read_temp[sizeof(write_temp)] = {0};
+
+	uint32_t read_id = 0;
+
+	/*调试串口配置*/
+	DEBUG_UART_Config();
+
+	/*调试串口配置*/
+	SPI_GPIO_Config();
+
+	printf("\r\n欢迎使用野火开发板\r\n");
+
+	read_id = SPI_FLASH_Read_JEDEC_ID();
+
+	printf("\r\nread_id = %x\r\n", read_id);
+	
+	/*擦除操作*/
+	SPI_FLASH_Erase_Sector(0);
+	
+	printf("\r\n擦除完毕\r\n");
+
+	/*写入操作*/
+	SPI_FLASH_Page_Write(0, (uint8_t*)&write_temp, sizeof(write_temp));
+	printf("\r\n写入完毕\r\n");
+
+	SPI_FLASH_Read_Buffer(0, read_temp, sizeof(write_temp));
+
+	printf("\r\n读取到的数据:\r\n");
+	printf("read_temp = %s", (char *)read_temp);
+	
+	while(1);
+}
+
+#endif
+
+#endif
+
+#endif
